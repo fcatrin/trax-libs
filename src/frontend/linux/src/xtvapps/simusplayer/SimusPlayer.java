8711c42221fbc7c3797b555f4a3da23c10355c25
Franco Catrin <fcatrin@gmail.com>
2020-04-22 21:31:29 -0400

    Library fixed for JNI

--- src/frontend/linux/src/xtvapps/simusplayer/SimusPlayer.java
+++ src/frontend/linux/src/xtvapps/simusplayer/SimusPlayer.java
@@ -3,9 +3,11 @@ package xtvapps.simusplayer;
 public class SimusPlayer {
 
 	public static void main(String[] args) {
+		/*
 		NativeInterface.alsaInit();
 		NativeInterface.alsaDumpPorts();
-		
+		*/
+
 		int handle = NativeInterface.midiLoad("/home/fcatrin/test.mid");
 		System.out.println(handle);
 	}
