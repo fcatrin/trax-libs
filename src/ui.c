55ca5e27c551495d9b31fc65464e91dc6631dbe2
Franco Catrin <fcatrin@gmail.com>
2020-04-27 02:33:51 -0400

    Test font rendering

--- src/ui.c
+++ src/ui.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #include <libs/nanovg/nanovg.h>
 #include <libs/nanovg/nanovg_gl.h>
+#include <common.h>
 
 static struct NVGcontext* vg;
 
@@ -9,6 +11,11 @@ void ui_init() {
 	vg = nvgCreateGLES2(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
 	if (!vg) {
 		printf("Could not init nanovg.\n");
+	}
+
+	int font = nvgCreateFont(vg, "sans", "/home/fcatrin/git/simusplayer/src/frontend/linux/fonts/Roboto-Regular.ttf");
+	if (font < 0) {
+		log_error("Could not add font");
 	}
 }
 
@@ -18,5 +25,18 @@ void ui_render(int width, int height) {
 	nvgRect(vg, 100,100, 120,30);
 	nvgFillColor(vg, nvgRGBA(255,192,0,255));
 	nvgFill(vg);
+
+	nvgSave(vg);
+
+	nvgFontSize(vg, 20.0f);
+	nvgFontFace(vg, "sans");
+	// nvgTextAlign(vg,NVG_ALIGN_CENTER|NVG_ALIGN_MIDDLE);
+
+	// nvgFontBlur(vg,2);
+	nvgFillColor(vg, nvgRGBA(0,0,0,192));
+	nvgText(vg, 50, 50, "Text Testing", NULL);
+
+	nvgRestore(vg);
+
 	nvgEndFrame(vg);
 }
