9eb640d8890f059017bc6ea9015253e641288ae9
Franco Catrin <fcatrin@gmail.com>
2020-05-28 20:31:51 -0400

    Bring libsimusplayer-mod.so

--- src/corelib/src/xtvapps/simusplayer/core/ModPlayer.java
+++ src/corelib/src/xtvapps/simusplayer/core/ModPlayer.java
@@ -1,6 +1,11 @@
 package xtvapps.simusplayer.core;
 
 public class ModPlayer extends Thread {
+	
+	static {
+		System.loadLibrary("simusplayer-mod");
+	}
+	
 	private native boolean xmpInit(String path, int freq);
 	private native void    xmpRelease();
 	private native int     xmpFillBuffer(byte[] buffer, int loop);
