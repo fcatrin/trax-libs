b667bd51e6ae154416d2c413c6e817fbfd26d056
Franco Catrin <fcatrin@gmail.com>
2020-05-29 22:21:11 -0400

    Use roland-like colors

--- src/corelib/src/xtvapps/simusplayer/core/widgets/WaveWidget.java
+++ src/corelib/src/xtvapps/simusplayer/core/widgets/WaveWidget.java
@@ -13,7 +13,7 @@ public class WaveWidget extends Widget {
 	private Color lineColor;
 	public WaveWidget(Window window) {
 		super(window);
-		lineColor = new Color("#FFFFFF");
+		lineColor = new Color("#333B50");
 	}
 
 	public void setWave(int wave[]) {
@@ -26,14 +26,19 @@ public class WaveWidget extends Widget {
 
 	@Override
 	protected void onPaint(PaintEvent e) {
-		if (wave== null) return;
 		Canvas canvas = e.canvas;
+		if (background != null) {
+			background.setBounds(bounds);
+			background.draw(canvas);
+		}
+		
+		if (wave== null) return;
 		canvas.setForeground(lineColor);
-		int px = bounds.x;
-		int py = bounds.y + bounds.height / 2;
+		int px = bounds.x+2;
+		int py = bounds.y+2 + (bounds.height-4) / 2;
 		for(int x=0; x<64 && x<wave.length; x++) {
-			int sample = wave[x] / 256 / 4;
-			canvas.drawLine(px, py - sample, px, py + sample);
+			int sample = wave[x] / 256 / 8;
+			canvas.drawLine(px, py - sample, px, py - sample+1);
 			px++;
 		}
 
@@ -41,7 +46,7 @@ public class WaveWidget extends Widget {
 
 	@Override
 	public Point getContentSize(int width, int height) {
-		return new Point(64, 64);
+		return new Point(68, 68);
 	}
 
 }
