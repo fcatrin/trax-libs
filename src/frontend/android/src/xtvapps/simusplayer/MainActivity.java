be593566396e87570b50596fd42d4334053e8d0a
Franco Catrin <fcatrin@gmail.com>
2020-06-05 18:48:17 -0400

    Create simusplayer Android project

---
+++ src/frontend/android/src/xtvapps/simusplayer/MainActivity.java
@@ -0,0 +1,12 @@
+package xtvapps.simusplayer;
+
+import fts.android.FtsActivity;
+
+public class MainActivity extends FtsActivity {
+
+	@Override
+	protected String getRootLayout() {
+		return "modplayer";
+	}
+
+}
