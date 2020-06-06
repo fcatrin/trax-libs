e3f5636421f1ff199fc3c226a41490345f110d14
Franco Catrin <fcatrin@gmail.com>
2020-06-05 20:20:48 -0400

    Make corelib buildable on Android

---
+++ src/corelib/jni/Android.mk
@@ -0,0 +1,26 @@
+# include $(call all-subdir-makefiles)
+
+LOCAL_PATH := $(call my-dir)
+
+#include $(CLEAR_VARS)
+
+#LOCAL_MODULE := xmp
+#LOCAL_SRC_FILES := $(SIMUSPLAYER_PATH)/../obj/local/$(TARGET_ARCH_ABI)/libxmp.a
+#include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := simusplayer-mod
+LOCAL_SRC_FILES := modplayer.cpp
+LOCAL_STATIC_LIBRARIES := xmp
+
+LOCAL_CFLAGS += -g -Wall -fPIC -I. -I$(SIMUSPLAYER_PATH)/libxmp/include
+ifeq ($(TARGET_ARCH),x86)
+#
+else
+	#LOCAL_CFLAGS +=  -marm -O3 -mfpu=neon
+endif 
+include $(BUILD_SHARED_LIBRARY)
+
+
