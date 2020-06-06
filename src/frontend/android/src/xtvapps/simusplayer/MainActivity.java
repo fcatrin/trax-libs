5cced1816a706abda5227b897b36a41172eaf1a7
Franco Catrin <fcatrin@gmail.com>
2020-06-05 21:07:26 -0400

    Display playing data

--- src/frontend/android/src/xtvapps/simusplayer/MainActivity.java
+++ src/frontend/android/src/xtvapps/simusplayer/MainActivity.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import fts.android.AndroidUtils;
 import fts.android.FtsActivity;
 import fts.core.Log;
+import fts.core.SimpleCallback;
 import fts.core.Utils;
 import fts.core.Window;
 import xtvapps.simusplayer.core.ModPlayer;
+import xtvapps.simusplayer.core.ModPlayer.FrameInfo;
 import xtvapps.simusplayer.core.ModPlayer.ModInfo;
 import xtvapps.simusplayer.core.lcd.LcdScreenWidget;
 import xtvapps.simusplayer.core.lcd.LcdSegmentWidget;
@@ -20,11 +22,11 @@ import xtvapps.simusplayer.core.widgets.WaveContainer;
 public class MainActivity extends FtsActivity {
 	private static final String LOGTAG = MainActivity.class.getSimpleName();
 
-	private WaveContainer waveContainer;
-	private LcdSegmentWidget lcdTime;
-	private LcdSegmentWidget lcdLength;
-	private LcdSegmentWidget lcdPosition;
-	private LcdSegmentWidget lcdTempo;
+	private static WaveContainer waveContainer;
+	private static LcdSegmentWidget lcdTime;
+	private static LcdSegmentWidget lcdLength;
+	private static LcdSegmentWidget lcdPosition;
+	private static LcdSegmentWidget lcdTempo;
 
 	private static ModPlayer modPlayer;
 	private static ModInfo modInfo;
@@ -42,7 +44,15 @@ public class MainActivity extends FtsActivity {
 			e1.printStackTrace();
 		}
 		
-		Window window = getFtsWindow();
+		final Window window = getFtsWindow();
+		
+		window.setOnFrameCallback(new SimpleCallback() {
+			
+			@Override
+			public void onResult() {
+				onFrameCallback();
+			}
+		});
 		
 		waveContainer = (WaveContainer)window.findWidget("waves");
 		
@@ -75,13 +85,11 @@ public class MainActivity extends FtsActivity {
 					System.out.println(String.format("%02X : %s", i, modPlayer.xmpGetSampleName(i)));
 				}
 				
-				/*
 				String modName = Utils.isEmptyString(modInfo.modName) ? "" : modInfo.modName;
 				
-				LcdSegmentWidget lcdModName = (LcdSegmentWidget)rootView.findWidget("lcdModName");
+				LcdSegmentWidget lcdModName = (LcdSegmentWidget)window.findWidget("lcdModName");
 				lcdModName.setText(toFirstLetterUppercase(modName));
 				waveContainer.setWaves(modInfo.tracks);
-				*/
 			}
 			
 			@Override
@@ -131,6 +139,51 @@ public class MainActivity extends FtsActivity {
 		
 		play();
 	}
+	
+	private static String padz(int n) {
+		if (n>99) n = 99;
+		return n < 10 ? "0" + n : "" + n;
+	}
+
+	private static String pads(String s, int n) {
+		s = "      " + s;
+		return s.substring(s.length()-n);
+	}
+
+	private static String buildTime(int t) {
+		t = t / 1000;
+		int seconds = t % 60;
+		int minutes = t / 60;
+		
+		return padz(minutes) + ":" + padz(seconds);
+	}
+	
+	protected static void onFrameCallback() {
+		if (modInfo == null) return;
+
+		for(int i=0; i<modInfo.tracks; i++) {
+			waveContainer.setWave(i, modPlayer.getWave(i));
+		}
+		
+		FrameInfo frameInfo = modPlayer.getFrameInfo();
+		
+		String elapsed = buildTime(frameInfo.time);
+		String length  = buildTime(frameInfo.totalTime);
+		lcdTime.setText(elapsed);
+		lcdLength.setText(length);
+		
+		String position = padz(frameInfo.position) + "/" + padz(modInfo.patterns);
+		lcdPosition.setText(position);
+		
+		String tempo = pads(String.valueOf(frameInfo.bpm), 5);
+		lcdTempo.setText(tempo);
+		
+	}
+	
+	private static String toFirstLetterUppercase(String s) {
+		return s.substring(0, 1).toUpperCase() + s.substring(1);
+	}
+
 
 	@Override
 	protected void onStart() {
